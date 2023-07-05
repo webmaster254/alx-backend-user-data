6880b2f1cf04f3761e80d9f365ba177c4fdf014d
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2023-07-05 22:24:17 +0300

    Update app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,9 +6,11 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -18,10 +20,16 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv("AUTH_TYPE", 'auth')
 
-if auth_type == "basic_auth":
+if auth_type == "session_auth":
+    auth = SessionAuth()
+elif auth_type == "basic_auth":
     auth = BasicAuth()
 elif auth_type == "auth":
     auth = Auth()
+elif auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -53,14 +61,18 @@ def before_request_handler():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            auth_header = auth.authorization_header(request)
+            session_cookie = auth.session_cookie(request)
+
+            if auth_header is None and session_cookie is None:
                 abort(401)
             if user is None:
                 abort(403)
+            request.current_user = user
 
 
 if __name__ == "__main__":
